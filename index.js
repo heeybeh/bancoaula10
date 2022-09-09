b16f65d03bfa590bc12d9376024ef7e1dc96fc54
Beatrice Fernandes <beatricefernandes@beatrices-air.lan>
2022-09-09 19:41:52 -0300

    second commit

--- index.js
+++ index.js
@@ -30,6 +30,69 @@ app.post('/person', async(req, res) => {
     }
 })
 
+app.get('/person', async (req, res) => {
+    try {
+        const people = await Person.find()
+        res.status(200).json(people)
+    } catch (error) {
+        res.status(500).json({erro:error})
+    }
+  
+})
+
+app.get('/person/:id', async(req, res) =>
+{
+    const id = req.params.id
+
+    try {
+        const person = await Person.findOne({_id:id})
+        if(!person) {
+            res.status(422).json({message:'User not found, sry'})
+            return
+        }
+        res.status(200).json(person)
+    } catch (error) {
+        res.status(500).json({erro:error})
+    }
+})
+app.patch('/person/:id', async(req, res) =>{
+    const id = req.params.id
+    const {name, age} = req.body
+
+    const person = {
+        name, age
+    }
+
+    try {
+        const updatedPerson = await Person.updateOne({_id:id}, person)
+            if(updatedPerson.matchedCount === 0) {
+                res.status(422).json({message:'User not founds'})
+                return
+            }
+            res.status(200).json(person)
+    }catch(error) {
+        res.status(500).json({erro:error})
+    }
+})
+
+app.delete('/person/:id', async(req,res) => {
+
+    const id = req.params.id
+    const person = await Person.findOne({_id:id})
+
+    if(!person) {
+        res.status(422).json({message:'User not found'})
+        return
+    }
+
+    try {
+        await Person.deleteOne({_id:id})
+        res.status(204).json({message:'Deleted User'})
+    } catch (error) {
+        res.status(500).json({erro:error})
+    }
+})
+
 mongoose.connect('mongodb+srv://novouser:fundatec01@cluster0.wlfwjli.mongodb.net/?retryWrites=true&w=majority')
 .then(()=> {
     console.log('Conectou')
